fa464c922e374439ad35cd7d966a313936b32d1c
y-serine <serine.in.sericin@gmail.com>
2013-09-20 17:48:22 +0900

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -9,13 +9,14 @@ int main(){
   
   GRAY_INT gNum2=SetBit("0010");//the internal value of gNum2 is 0x2. (it means 3 in Gray code expression)
   
-  gNum1+gNum2;//the internal value of gNum2 is 0x5. (it means 6 in Gray code expression)
+  gNum1+gNum2;//the internal value of the answer is 0x7. (it means 5 in Gray code expression)
+  gNum1-gNum2;//the internal value of the answer is 0x1. (it means 1 in Gray code expression)
+  gNum1&gNum2;//the internal value of the answer is 0x2. (it means 3 in Gray code expression)
+  gNum1++;//the internal value of gNum1 is 0x6. (it means 4 in Gray code expression)
   
-  //in calculation, I translate the Gray codes into binary expression in default. 
-  //You will be allowed to choose Gray code calculation in the future. 
-  
-  Addition2(gNum1,gNum2);//half translation (ten times slower)
-  Addition6(gNum1,gNum2);//Gray code calculation (ten times slower)
+  //in calculation, I translated the Gray codes into binary expression before. 
+  //However, you can use the Gray code calculation now. 
+  //multiplication and devision are not implemented yet.
   
   
   return 0;
